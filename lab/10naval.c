57c784afc3adf24faf6f36cedef2a214baf2533e
Fernando Valentim Torres <chereibos@gmail.com>
2024-05-14 17:35:09 -0300

    Comparador de magnitude

---
+++ lab/10naval.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+//Num = -48
+//Alfabeto Maiusculo = -65
+
+int main(){
+    int n; scanf(" %d", &n);
+    char mar[n][n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+            scanf(" %c", &mar[i][j]);
+        }
+    }
+    int a; scanf(" %d", &a);
+    char coord[4];
+    for(int i = 0; i < a; i++) {
+        int x, y, z;
+        scanf(" %s", coord);
+        x = coord[0]-65;
+        y = coord[1]-49;
+        z = coord[2]-48;
+        if (z >= 0) y = z + 9 + (10 * y);
+        if(mar[x][y] == 'N') {
+            printf("acerto!\n");
+            mar[x][y] = 'X';
+        } else {
+            printf("erro!\n");
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+            printf("%c ", mar[i][j]);
+        }
+        printf("\n");
+    }
+}
