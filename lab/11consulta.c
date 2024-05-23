143e6d034393c1727d964147c7ed69c46a9faa1a
fernandovtorres <chereibos@gmail.com>
2024-05-23 09:07:13 -0300

    leroy merlin

--- lab/11consulta.c
+++ lab/11consulta.c
@@ -28,35 +28,53 @@ int somadivisoresprimos(int codigo) {
     return sum;
 }
 
-int particaoo(dado *a, int low, int high) {
-    int pivo = a[low].ip;
-    int i = low;
-    int j = high;
-    while(i < j) {
-        while(a[i].ip <= pivo && i <= high - 1) {
-            i ++;
-        }
-        while(a[j].ip > pivo && j >= low + 1) {
-            j--;
-        }
-        if (i < j) {
-            dado temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+void merge(dado *esq, dado *dir, dado *a, int tam) {
+    int esqsize = tam/2;
+    int dirsize = tam - esqsize;
+    int i = 0, l = 0, r = 0;
+
+    while (l < esqsize && r < dirsize) {
+        if(esq[l].ip > dir[r].ip) { // só trocar para alterar a ordem
+            a[i] = esq[l];
+            i++;
+            l++;
+        } else {
+            a[i] = dir[r];
+            i++;
+            r++;
         }
     }
-    dado temp = a[low];
-    a[low] = a[j];
-    a[j] = temp;
-    return j;
+    while (l < esqsize) {
+        a[i] = esq[l];
+        i++;
+        l++;
+    }
+    while (r < dirsize) {
+        a[i] = dir[r];
+        i++;
+        r++;
+    }
 }
 
-void sort(dado a[], int low, int high) {
-    if (low < high) {
-        int particao = particaoo(a, low, high);
-        sort(a, low, particao-1);
-        sort(a, particao+1, high);
+void mergesort(dado a[], int tam) {
+    if(tam <= 1) return;
+    int meio = tam/2;
+    dado arresq[meio];
+    dado arrdir[tam-meio];
+
+    int i = 0;
+    int j = 0;
+    for(; i < tam; i++){
+        if(i < meio) {
+            arresq[i] = a[i];
+        } else {
+            arrdir[j] = a[i];
+            j++;
+        }
     }
+    mergesort(arresq, meio);
+    mergesort(arrdir, tam-meio);
+    merge(arresq, arrdir, a, tam);
 }
 
 int main() {
@@ -71,7 +89,7 @@ int main() {
             dados[i].conteudo = (char *)calloc(maxtam + 1, sizeof(char));
             scanf("%s", dados[i].conteudo);
         }
-        sort(dados, 0, n-1);
+        mergesort(dados, n);
         for (int i = 0; i < n; i++) {
             printf("%s %d\n", dados[i].conteudo, dados[i].codigo);
         }
