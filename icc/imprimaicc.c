4ef52dac81408d49fac52e652d7578549fae191f
fernandovtorres <chereibos@gmail.com>
2024-06-13 16:44:16 -0300

    sei laaaaaa

--- icc/imprimaicc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    unsigned long long int n1;
-    scanf("%llu", &n1);
-    printf("%llu\n", n1);
-    return (0);
-}
--- lab/2Formatarlab.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char a[8];
-    scanf("%s", a);
-    printf("%.2s/%.2s/%.4s", a, a+2, a+4);
-}
