3dd89503de3f1caf693dc94538e17d1ec34418c8
Fernando Valentim Torres <54219097+fernandovtorres@users.noreply.github.com>
2024-06-29 22:47:29 -0300

    Create 15somar.a

---
+++ Lab de ICC/lab/15somar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int count(int n){
+    if(n == 1){
+        return 1;
+    } else if(n == 0){
+        return 0;
+    }
+    n-=1;
+    return count(n) + 1;
+}
+
+int soma(int n, int m){
+    return count(n) + count(m);
+}
+
+int main(){
+    int n, m; scanf(" %d %d", &n, &m);
+    printf("%d", soma(n, m));
+    return 0;
+}
